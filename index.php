1ded19802766c52c2160146a4f69cabb18cbcb39
Sam Andre <samyrocks1606@gmail.com>
2012-10-18 02:41:17 +0530

    Update index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<table align="center" cellpadding="0" cellspacing="0">
+<tr><td align="center"><h1>WWW.PROJ-SAM.COM</h1></td></tr>
+<tr>
+<td>
+<div class="c1"><br><br><br><br><br><a href="#"><h1>HOME1</h1></a></div>
+<div class="c2"><br><br><br><br><br><a href="#"><h1>HOME2</h1></a></div>
+</td></tr>
+<tr>
+<td>
+<div class="c3"><br><br><br><br><br><a href="#"><h1>HOME3</h1></a></div>
+<div class="c4"><br><br><br><br><br><a href="#"><h1>HOME4</h1></a></div>
+</td></tr>
+</table>
+</body>
+</html>
